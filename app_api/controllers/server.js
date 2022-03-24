6e3e7f378e81506e4fa882b173474fc65cd6dd82
Meshva1005 <ptlmeshva10@gmail.com>
2022-03-24 00:20:37 -0400

    first push

---
+++ app_api/controllers/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const routesUrls = require('../routes/register')
+const cors = require('cors')
+
+dotenv.config()
+mongoose.connect("mongodb+srv://Meshva:Meshva@cluster0.l32qe.mongodb.net/mytable?retryWrites=true&w=majority", () => console.log("Database connected..!!"))
+
+app.use(express.json())
+app.use(cors())
+// append server.js to routes.js
+app.use('/app', routesUrls)
+app.listen(4000, () => console.log("Server is up and running"))
---
+++ app_public/src/pages/index.js
@@ -0,0 +1,13 @@
+
+export { default as About }
+from './about_us/About';
+export { default as Discover }
+from './discover/Discover';
+export { default as Home }
+from './home/Home';
+export { default as Login }
+from './login/Login';
+export { default as Product_page }
+from './product_page/Product_page';
+export { default as Signup }
+from './signup/SignUp';
