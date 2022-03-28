5b493053da36423d47a9ea227cc4ec3d751922bb
Nishant Ajay Jagtap <jerry.jagtap5@gmail.com>
2022-03-28 18:34:47 -0400

    Delete Nishant directory

--- Nishant/app_public/src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as CTA } from './cta/CTA';
-export { default as Navbar } from './navbar/Navbar';
--- Nishant/app_public/src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Card } from '../components/card/Card';
-export { default as Features } from './features/Features';
-export { default as Footer } from '../components/footer/Footer';
-export { default as Hero } from './hero/Hero';
-export {default as Carousel} from './carousel/Carousel'
