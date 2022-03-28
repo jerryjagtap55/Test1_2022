81820cb220bf14852f514fe2e4e31a43ddba21de
Nishant Ajay Jagtap <jerry.jagtap5@gmail.com>
2022-03-28 18:26:37 -0400

    Delete Nishant directory

--- Nishant/app_public/README.old.md
+++
@@ -1,2 +0,0 @@
-# react_test
- testing react project as dumy project
--- Nishant/app_server/controllers/main.js
+++
@@ -1,8 +0,0 @@
-// Index Page Controllor 
-const index = function(req, res, next) {
-    res.render('index', { title: 'Express' });
-};
-
-module.exports = {
-    index
-}
