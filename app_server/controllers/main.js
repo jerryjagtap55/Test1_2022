168d91c528c869c3a4c0e5293b0e996db42f0064
Nishant Ajay Jagtap <jerry.jagtap5@gmail.com>
2022-04-01 16:08:35 -0400

    Update main.js

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 // Index Page Controllor 
 const index = function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Dream Catcher on Express' });
 };
 
 module.exports = {
