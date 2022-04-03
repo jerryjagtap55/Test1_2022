d339fda669be49bfc81566d21ddfd80e70b71a26
Nishant Ajay Jagtap <jerry.jagtap5@gmail.com>
2022-04-03 16:15:03 -0400

    check-out form

--- Nishant/app_public/src/App.js
+++ Nishant/app_public/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CTA, Navbar, Card } from './components/index';
 import { Features, Footer } from './containers/index';
-import {About, Discover, Home, Login, Product_page, Signup, Dashboard, Sell } from './pages/index';
+import {About, Discover, Home, Login, Product_page, Signup, Dashboard, Sell, Checkout } from './pages/index';
 import './App.css';
 
 
@@ -22,6 +22,8 @@ const App = () => {
         <Route path="/product" element={<Product_page/>} />
         <Route path='/user' element = {<Dashboard/>} />
         <Route path='/sell' element = {<Sell/>} />
+        <Route path='/checkout' element = {<Checkout/>} />
+        
       </Routes>
       <div>
       <Footer />
