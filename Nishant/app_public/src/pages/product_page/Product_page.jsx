beb0c35709a7dcc6651a7173d4aa755b3cdb396a
militadabhi <dabhi.milita@gmail.com>
2022-04-03 22:41:03 -0400

    Update Product_page.jsx

--- Nishant/app_public/src/pages/product_page/Product_page.jsx
+++ Nishant/app_public/src/pages/product_page/Product_page.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+// Actions
+import { loadAdDetails, loadAdImage, setImageLoadingStatus } from '../../actions/ad';
 import {
     FormControl,
     InputLabel,
@@ -10,72 +14,99 @@ import {
 import './product_page.css';
 import DoneIcon from '@mui/icons-material/Done';
 import card_img from '../../assets/art5.jpg';
+import { secondsToHmsShort } from '../../../utils/secondsToHms';
 
-const Product_page = () => {
+const Product_page = ( props ) => {
+    const navigate = useNavigate();
 
-    // event handeler
-    function onSubmit() {
-        var amount = document.getElementById('amount').value;
-        var password = document.getElementById('password').value;
+    const handleCardClick = ( e ) => {
+        navigate( `/ads/${ props.ad._id }` );
 
-    }
+    };
+
+    const updateAuctionStatus = ( ad ) => {
+        if ( ad.sold ) {
+            return 'Sold';
+        } else if ( ad.auctionEnded ) {
+            return 'Ended, not-sold';
+        } else if ( !ad.auctionStarted ) {
+            return 'Upcoming';
+        } else {
+            return 'Ongoing';
+        }
+    };
+
+
 
     return (
         <>
-            <div className='text-center my-5'>
-                <div className="card mb-3">
+            <a
+                onClick={( e ) => {
+                    handleCardClick( e );
+                }}
+                style={{ textDecoration: 'none' }}
+            >
+                <div className='text-center my-5'>
+                    <div className="card mb-3">
 
-                    <div className="card-body row">
-                        <div className="col-md-7 card_img-holder">
-                            <img src={card_img} className="img-fluid rounded-start product_img" alt="Card" />
-                        </div>
-                        
-                        <div className="col-md-5">
-                            <div>
-                                <h4 className='card-title display-6'>Card Title</h4>
-                                <h6 className='card-subtitle mb-2 text-muted'>
-                                    by User
-                                </h6>
-                                <p className="card-text small">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div className="card-body row">
+                            <div className="col-md-7 card_img-holder">
+                                <img src={props.ad.image ? props.ad.image : imagePlaceholder} className="img-fluid rounded-start product_img" alt="Card" />
+                            </div>
+
+                            <div className="col-md-5">
+                                <div>
+                                    <h4 className='card-title display-6'>{props.ad.productName}</h4>
+                                    <h6 className='card-subtitle mb-2 text-muted'>
+                                        by User don't know how to call user name
+                                    </h6>
+                                    <p className="card-text small">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 
 
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">1st Bid!</li>
-                                    <li className="list-group-item">Current Bid!</li>
-                                    <li className="list-group-item"></li>
-                                </ul>
+                                    <ul className="list-group list-group-flush">
+                                        <li className="list-group-item">Current Bid! {props.ad.currentPrice.$numberDecimal}</li>
+                                    </ul>
 
-                                <div className='d-flex align-items-baseline justify-content-between'>
-                                    <p className='lead'>
-                                        Place Your bid :
+                                    <div className='d-flex align-items-baseline justify-content-between'>
+                                        <p className='lead'>
+                                            Place Your bid :
+                                        </p>
+                                        <FormControl sx={{ m: 1 }} variant="standard">
+                                            <Input
+                                                size="small"
+                                                id="amount"
+                                                startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                            />
+                                        </FormControl>
+                                        <IconButton type='submit' onClick={() => onSubmit()} aria-label="submit bid">
+                                            <DoneIcon />
+                                        </IconButton>
+
+                                    </div>
+                                    <p className='card-text lead text-muted my-4'>
+                                        Remaining: {secondsToHmsShort( props.ad.timer )}
                                     </p>
-                                    <FormControl sx={{ m: 1 }} variant="standard">
-                                        <Input
-                                            size="small"
-                                            id="amount"
-                                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                                        />
-                                    </FormControl>
-                                    <IconButton type='submit' onClick={() => onSubmit()} aria-label="submit bid">
-                                        <DoneIcon />
-                                    </IconButton>
-
                                 </div>
-                                <p className='card-text lead text-muted my-4'>
-                                    Timer:  1Day  20hrs  10mins
-                                </p>
                             </div>
                         </div>
+
+                        <div className="card-footer text-muted small text-start">
+                            Status: {updateAuctionStatus( props.ad )}
+                        </div>
                     </div>
 
-                    <div className="card-footer text-muted small text-start">
-                        2 days ago
-                    </div>
                 </div>
-
-            </div>
+            </a>
         </>
-    )
-}
+    );
+};
 
-export default Product_page
+const mapStateToProps = ( state ) => ( {
+    adDetails: state.ad.adDetails,
+} );
+
+export default connect( mapStateToProps, {
+    loadAdDetails,
+    loadAdImage,
+    setImageLoadingStatus,
+} )( Product_page );
